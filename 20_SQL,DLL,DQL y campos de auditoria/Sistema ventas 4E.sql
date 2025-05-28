f7c466ef1ee5b6ec9e1818f99f2eaf7045eee25e
AlexanderRiquelme <alexander.riquelme676@gmail.com>
2025-05-28 12:38:19 -0400

    base

--- 20_SQL,DLL,DQL y campos de auditoria/Sistema ventas 4E.sql
+++ 20_SQL,DLL,DQL y campos de auditoria/Sistema ventas 4E.sql
@@ -1,65 +1,37 @@
--- Crear la base de datos
-CREATE DATABASE sistema_ventas;
--- Seleccionar la base de datos para trabajar
-USE sistema_ventas;
-
--- Crear tabla tipo_usuario (estructura)
-
--- SQL:
-
+create database sistema_ventas_4E;
+USE sistema_ventas_4E;
 -- Creamos la tabla tipo_usuario
 CREATE TABLE tipo_usuarios (
-id_tipo_usuario INT AUTO_INCREMENT PRIMARY KEY,
+id INT AUTO_INCREMENT PRIMARY KEY,
 -- Identificador único
 nombre_tipo VARCHAR(50) NOT NULL,
 -- Tipo de usuario (Admin, Cliente)
-
 -- Campos de auditoría
-
 created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 -- Fecha creación
-
 updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
 ON UPDATE CURRENT_TIMESTAMP, -- Fecha modificación
-
 created_by INT,-- Usuario que crea
-
 updated_by INT,-- Usuario que modifica
-
 deleted BOOLEAN DEFAULT FALSE -- Borrado lógico
-
 );
+
 -- Tabla para usuarios
 
 CREATE TABLE usuarios (
-
 id_tipo_usuario INT AUTO_INCREMENT PRIMARY KEY, -- Id único
-
-nombre VARCHAR(100) NOT NULL, -- Nombre de usuario
-
-email VARCHAR(100) UNIQUE, -- Correo electrónico único
-
-password VARCHAR(32) NOT NULL,
-
+nombre_tipo VARCHAR(100) NOT NULL, -- Nombre de usuario
+correo VARCHAR(100) UNIQUE, -- Correo electrónico único
 tipo_usuario_id INT, -- Relación a tipo_usuario
-
 -- Campos de auditoría
-
 created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 -- Fecha creación
-
 updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
 ON UPDATE CURRENT_TIMESTAMP, -- Fecha modificación
-
 created_by INT,-- Usuario que crea
-
 updated_by INT,-- Usuario que modifica
-
 deleted BOOLEAN DEFAULT FALSE -- Borrado lógico
-
 );
-
--- Relación (fk usuario – tipo_usuario)
 
 ALTER TABLE usuarios -- Modificar tabla
 -- Agregar una restricción (FK)
@@ -69,4 +41,38 @@ ADD CONSTRAINT fk_usuario_tipo_usuario
 -- Añade referencia(FK)
 
 FOREIGN KEY (tipo_usuario_id) REFERENCES
-tipo_usuarios(id_tipo_usuario);
+tipo_usuarios(id);
+
+CREATE TABLE productos (
+id_producto INT,
+nombre VARCHAR(50) NOT NULL,
+precio FLOAT,
+stock INT,
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+ON UPDATE CURRENT_TIMESTAMP,
+created_by int,
+updated_by int,
+deleted BOOLEAN DEFAULT FALSE
+);
+create table ventas (
+id_venta INT AUTO_INCREMENT PRIMARY KEY,
+fecha DATETIME DEFAULT CURRENT_TIMESTAMP,
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+ON UPDATE CURRENT_TIMESTAMP,
+created_by int,
+updated_by int,
+deleted BOOLEAN DEFAULT FALSE
+);
+create table detalle_ventas (
+id_detalle INT AUTO_INCREMENT PRIMARY KEY,
+cantidad int,
+precio_unitario bigint,
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+ON UPDATE CURRENT_TIMESTAMP,
+created_by int,
+updated_by int,
+deleted BOOLEAN DEFAULT FALSE
+);
